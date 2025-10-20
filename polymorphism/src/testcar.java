e1ba7bcd736f2e5372e17d561a7a73a69608d56f
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-10-20 18:11:39 +0300

    Commit 1
    In the new version on line 11, I Changed car object's speed from 000 to 0.
    
    In the new version on line 17and24, I added more dashes.
    
    In the new version on line 18,I changed speed from 500km/h to 200km/h and  added speed limit of 400km/h
    
    In the new version on line 25,I updated the speed from 550km/h to 150km/h and added speed limit of 300km/h

--- polymorphism/src/testcar.java
+++ polymorphism/src/testcar.java
@@ -8,21 +8,21 @@ public class testcar {
 
     public static void main(String[] args) {
         System.out.println("--------Car--------");
-        Car car=new Car("Vehicle",":000");
+        Car car=new Car("Vehicle",":000","0");
         car.start();
         car.displayInfo("Made in ??");
         testcar(car);
         System.out.println();
 
-        System.out.println("-----Electric car----");
-        electricCar electric=new electricCar("Hybrid","500km/h","beeps on start");
+        System.out.println("-----Electric car------");
+        electricCar electric=new electricCar("Hybrid","200km/h","beeps on start","400km/h");
         electric.start();
         electric.displayInfo("Made in UK");
         testcar(electric);
 
         System.out.println();
-        System.out.println("-------Sports car");
-        SportsCar sports=new SportsCar("Monster Track","550km/h","1st");
+        System.out.println("-------Sports car-------");
+        SportsCar sports=new SportsCar("Monster Track","150km/h","1st","David.J","300km/h");
         sports.start();
         sports.displayInfo("Made in America");
         testcar(sports);
