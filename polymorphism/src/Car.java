fd6fdf19a048d9114176f334527b92ea68d6b6bb
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-10-20 17:01:14 +0300

    Commit 1
    In the new version on line 4; and 6, I  created anew insatance variable
    
    In the new version on line 14, I added a new string Km/h.
    
    In the new version on line 16 and 19, a new parameter and string called speed limit

--- polymorphism/src/Car.java
+++ polymorphism/src/Car.java
@@ -1,20 +1,22 @@
 public class Car {
     protected  String brand;
     protected  String speed;
+    protected String speedlimit;
 
-    public Car(String brand, String speed) {
+    public Car(String brand, String speed,String speedlimit) {
         this.brand = brand;
         this.speed = speed;
+        this.speedlimit=speedlimit;
     }
     public  void start() {
         System.out.println("Car:"+" "+brand + " " + "engine is starting ");
     }
 
     public void displayInfo() {
-        System.out.println("Car:" +" "+ brand + " "  + "speed:"+" "+speed + "km/h");
+        System.out.println("Car:" +" "+ brand + " "  + "speed:"+" "+speed + " "+"Speed limit:"+" "+speedlimit);
     }
     public void displayInfo(String OtherInformation) {
-        System.out.println("Car:" + brand + " " +   speed);
+        System.out.println("Car:" + brand + " " +   speed+" "+"Speed limit:"+" "+speedlimit+"km/h");
 
     }
 
