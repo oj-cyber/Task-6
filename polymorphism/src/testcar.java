3374212bd795502a00c4210515151c3e69c96342
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-10-20 18:40:41 +0300

    Commit 2
    In the new version on line 3,I changed string Car testing to demostrating real time polymorphsim.
    In the new version on line 11,added speed limit 0km/h.
    In the new version on line 18 ,changed speed from 500km/h to 200km/h and added speed limit of 400km/h.
    In the new version on line  25 changed speed from 550km/h to 150km/h and added new of speed limit 300km/h

--- polymorphism/src/testcar.java
+++ polymorphism/src/testcar.java
@@ -1,6 +1,6 @@
 public class testcar {
     public static void testcar(Car car){
-        System.out.println("Car testing:");
+        System.out.println("Demostrating real time polymorphsim:");
         car.start();
         car.displayInfo();
 
@@ -8,23 +8,23 @@ public class testcar {
 
     public static void main(String[] args) {
         System.out.println("--------Car--------");
-        Car car=new Car("Vehicle",":000");
+        Car car=new Car("Vehicle",":000","0");
         car.start();
         car.displayInfo("Made in ??");
         testcar(car);
         System.out.println();
 
         System.out.println("-----Electric car----");
-        electricCar electric=new electricCar("Hybrid","500km/h","beeps on start");
+        electricCar electric=new electricCar("Hybrid","200km/h","beeps on start","400");
         electric.start();
         electric.displayInfo("Made in UK");
         testcar(electric);
 
         System.out.println();
         System.out.println("-------Sports car");
-        SportsCar sports=new SportsCar("Monster Track","550km/h","1st");
+        SportsCar sports=new SportsCar("Monster Track","150km/h","1st","david.J","300km/h");
         sports.start();
-        sports.displayInfo("Made in America");
+        sports.displayInfo("Made in America ");
         testcar(sports);
     }
 
